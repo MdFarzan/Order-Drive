697ea9b7c8a9ad544c3a959deb8bac87f86711fe
Md Farzan <mdfarzan776@gmail.com>
2021-05-16 15:45:03 +0530

    session checking added before redirection.

--- app/Controllers/SiteManagement.php
+++ app/Controllers/SiteManagement.php
@@ -3,6 +3,9 @@ namespace App\Controllers;
 
 class SiteManagement extends BaseController{
     function index(){
+        if((checkSession('credentials')==false && checkSession('privileges')==false)){
+            return redirect()->to(site_url());
+        }
         return view('Master Layouts/Admin Layout/starter.php');
     }
 }

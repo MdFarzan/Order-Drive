f1d35b0ccdd52fc00e8617678c558450d0cb33cf
Md Farzan <mdfarzan776@gmail.com>
2021-05-17 11:32:00 +0530

    previous session checking added on index/sign-in page.

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -10,6 +10,9 @@ class Home extends BaseController
 	// }
 
 	public function index(){
+		if(!(checkSession('credentials')==false && checkSession('privileges')==false)){
+            return redirect()->to('site-management/');
+        }
 		return view('index');
 	}
 }

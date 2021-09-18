739edeb1d2aa974973016b5214b5340ea9a898d6
Md Farzan <mdfarzan776@gmail.com>
2021-09-18 13:17:56 +0530

    file: model added for category-management.

---
+++ app/Models/CategoryManagementModel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class CategoryManagementModel extends Model
+{
+	protected $DBGroup              = 'default';
+	protected $table                = 'od_product_categories';
+	protected $primaryKey           = 'id';
+	protected $useAutoIncrement     = true;
+	// protected $insertID             = 0;
+	protected $returnType           = 'array';
+	protected $useSoftDelete        = false;
+	protected $protectFields        = true;
+	protected $allowedFields        = ['id', 'p_id', 'name', 'slug'];
+
+	// Dates
+	protected $useTimestamps        = false;
+	protected $dateFormat           = 'datetime';
+	protected $createdField         = 'created_at';
+	protected $updatedField         = 'updated_at';
+	protected $deletedField         = 'deleted_at';
+
+	// Validation
+	protected $validationRules      = [];
+	protected $validationMessages   = [];
+	protected $skipValidation       = true;
+	protected $cleanValidationRules = true;
+
+	// Callbacks
+	protected $allowCallbacks       = false;
+	protected $beforeInsert         = [];
+	protected $afterInsert          = [];
+	protected $beforeUpdate         = [];
+	protected $afterUpdate          = [];
+	protected $beforeFind           = [];
+	protected $afterFind            = [];
+	protected $beforeDelete         = [];
+	protected $afterDelete          = [];
+}

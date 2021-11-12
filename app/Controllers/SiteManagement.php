73ff08b2fc2ab2de80715100990741061b001100
Md Farzan <mdfarzan776@gmail.com>
2021-11-12 09:01:48 +0530

    feature: admin dashboard added.

--- app/Controllers/SiteManagement.php
+++ app/Controllers/SiteManagement.php
@@ -1,12 +1,30 @@
 <?php
 namespace App\Controllers;
+use App\Models\OrderModel;
 
 class SiteManagement extends BaseController{
     function index(){
         if((checkSession('credentials')==false && checkSession('privileges')==false)){
             return redirect()->to(site_url());
         }
-        return view('Admin Views/Dashboard');
+
+        $order_db = new OrderModel();
+			$all_orders = count($order_db->findAll());
+			$cancel_orders = count($order_db->where('order_status', '0')->findAll());
+			$just_placed = count($order_db->where('order_status', '1')->findAll());
+			$under_processing = count($order_db->where('order_status', '2')->findAll());
+			$under_shipping = count($order_db->where('order_status', '3')->findAll());
+			$delivered_orders = count($order_db->where('order_status', '4')->findAll());
+
+			
+			$data['all_orders'] = $all_orders;
+			$data['cancel_orders'] = $cancel_orders;
+			$data['just_placed'] = $just_placed;
+			$data['under_processing'] = $under_processing;
+			$data['under_shipping'] = $under_shipping;
+			$data['delivered_orders'] = $delivered_orders;
+
+        return view('Admin Views/Dashboard', $data);
     }
 
     
--- app/Views/Admin Views/Dashboard.php
+++ app/Views/Admin Views/Dashboard.php
@@ -1,5 +1,8 @@
 <?=$this->extend('Master Layouts/Admin Layout/starter');
 $this->section('main_content');
+
+
+
 ?>
 
     <!-- Content Header (Page header) -->
@@ -11,7 +14,7 @@ $this->section('main_content');
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Site Management</a></li>
+              <li class="breadcrumb-item"><a href="#">Order Drive</a></li>
               <li class="breadcrumb-item active">Dashboard</li>
             </ol>
           </div><!-- /.col -->
@@ -23,64 +26,114 @@ $this->section('main_content');
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
 
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
+        
 
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+        <!-- stat box starts -->
+        <div class="row justify-content-center">
+
+           <!-- ./col -->
+           <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box" style="background:#f4da16;">
+              <div class="inner">
+                <h3><?php echo $just_placed; ?></h3>
+
+                <p>New Orders</p>
               </div>
-            </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
               </div>
-            </div><!-- /.card -->
-          </div>
-          <!-- /.col-md-6 -->
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
+              <a href="<?php echo base_url('site-management/new-orders/'); ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
-          <!-- /.col-md-6 -->
+          <!-- ./col -->
+
+         
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3><?php echo $delivered_orders; ?><sup style="font-size: 20px"></sup></h3>
+
+                <p>Delivered Orders</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="<?php echo base_url('site-management/all-orders/'); ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3><?php echo $under_shipping; ?></h3>
+
+                <p>Under Shipping</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="<?php echo base_url('site-management/all-orders/'); ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3><?php echo $under_processing; ?></h3>
+
+                <p>Under Processing</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="<?php echo base_url('site-management/all-orders/'); ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+        
+          <div class="col-lg-3 col-6">
+          
+          <!-- small box -->
+          <div class="small-box bg-primary">
+            <div class="inner">
+              <h3><?php echo $all_orders; ?></h3>
+
+              <p>All Orders</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="<?php echo base_url('site-management/all-orders/'); ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
         </div>
-        <!-- /.row -->
+        <!-- ./col -->
+
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3><?php echo $cancel_orders; ?></h3>
+
+                <p>Canceled Orders</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="<?php echo base_url('site-management/all-orders/'); ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+
+        </div>
+        <!-- stat box ends -->
+
+        
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
